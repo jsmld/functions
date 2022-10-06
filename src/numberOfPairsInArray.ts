a1b7e953347c758f4a7c6aba0bde68f7762a723b
Sam van Erpers Roijaards <samvanerpersroijaards@gmail.com>
2022-10-06 17:27:12 +1300

    Refactors

--- src/numberOfPairsInArray.ts
+++ src/numberOfPairsInArray.ts
@@ -4,9 +4,9 @@ const numberOfPairsInArray = (arr: number[]): number => {
     pairs[el] ||= 0
     pairs[el] += 1
   })
-  const pairCount = Object.values(pairs).map(val => Math.floor(val/2))
+  const pairCount = Object.values(pairs).map(val => Math.floor(val / 2))
   const totalNumberOfPairs = pairCount.reduce((sum, x) => sum += x)
-  return totalNumberOfPairs;
+  return totalNumberOfPairs
 }
 
-export default numberOfPairsInArray;
+export default numberOfPairsInArray
